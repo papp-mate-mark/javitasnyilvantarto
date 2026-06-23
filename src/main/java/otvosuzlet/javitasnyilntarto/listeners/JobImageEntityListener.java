50c62ea17e254b4049ef206647c3cc76025f1190
papp-mate-mark <datapex73@gmail.com>
2026-06-23 14:45:26 +0200

    Handle IO exception with a warn instead.

--- src/main/java/otvosuzlet/javitasnyilntarto/listeners/JobImageEntityListener.java
+++ src/main/java/otvosuzlet/javitasnyilntarto/listeners/JobImageEntityListener.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 import jakarta.persistence.PostRemove;
 import otvosuzlet.javitasnyilntarto.model.JobImage;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -23,15 +24,15 @@ public class JobImageEntityListener {
         if (jobImage.getImageFilename() != null) {
             try {
                 Files.deleteIfExists(Path.of(imageDirectory, jobImage.getImageFilename()));
-            } catch (Exception e) {
-                logger.error("Error occurred while deleting image file: {}. Trace: {}", jobImage.getImageFilename(), e.getMessage(), e);
+            } catch (IOException e) {
+                logger.warn("Could not delete image file: {}. {}", jobImage.getImageFilename(), e.getMessage());
             }
         }
         if (jobImage.getThumbnailFilename() != null) {
             try {
                 Files.deleteIfExists(Path.of(imageDirectory, jobImage.getThumbnailFilename()));
-            } catch (Exception e) {
-                logger.error("Error occurred while deleting thumbnail file: {}. Trace: {}", jobImage.getThumbnailFilename(), e.getMessage(), e);
+            } catch (IOException e) {
+                logger.warn("Could not delete thumbnail file: {}. {}", jobImage.getThumbnailFilename(), e.getMessage());
             }
         }
     }
